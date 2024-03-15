d670547d54566c901a08175f23d12ca9d134160d
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-03-15 11:24:04 -0300

    ok

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ for (let i=0; i<botoes.length; i++){
             texto[j].classList.remove("ativo");
         }
 
-        botoes[i].classList.add("ativo");    }
+        botoes[i].classList.add("ativo");
+    }
     
 }
