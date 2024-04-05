6607cb91511bff551efcc07b2c9694d2efc78656
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-04-05 11:31:52 -0300

    ok

--- main.js
+++ main.js
@@ -35,6 +35,12 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
     
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
+
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
 
