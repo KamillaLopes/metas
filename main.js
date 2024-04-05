7d34c53368ace792cf0bc4a8f7ddccdaf4240eed
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-04-05 11:26:20 -0300

    ok

--- main.js
+++ main.js
@@ -24,10 +24,13 @@ const tempoObjetivo4 = new Date("2024-12-31T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function atualizaCronometro(){
-    for (let i=0; i<contadores.length; i++){
+    for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
+
+atualizaCronometro();
+setInterval(atualizaCronometro(),1000);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
