69323bf4ea4b4301717eb710bd407f2d9d023c25
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-04-08 10:15:50 -0300

    ok

--- main.js
+++ main.js
@@ -40,8 +40,6 @@ function calculaTempo(tempoObjetivo){
     } else {
         return "Prazo Finalizado";
     }
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
 
 function atualizaCronometro(){
