7e565e0c838a6354cb5a7b020df06dbec2ce3142
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-04-08 10:23:44 -0300

    ok

--- main.js
+++ main.js
@@ -50,5 +50,7 @@ function atualizaCronometro(){
 
 function comecaCronometro(){
 atualizaCronometro();
-setInterval(atualizaCronometro(),1000);
+setInterval(atualizaCronometro,1000);
 }
+
+comecaCronometro();
