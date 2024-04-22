cb71fbd2a91b7af7dc6468d26f72ded45d963b0f
Kamilla Lopes <105080698+KamillaLopes@users.noreply.github.com>
2024-04-22 10:07:06 -0300

    ok

--- main.js
+++ main.js
@@ -35,17 +35,14 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
     
     if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos];
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return [0,0,0,0];
+        return "Prazo Finalizado";
     }
+
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];  
-    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
     for (let i=0; i<contadores.length;i++){
     contadores[i].textContent = calculaTempo(tempos[i]);
     }
